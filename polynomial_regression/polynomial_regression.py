2b41006ad0b55a2a9d8f4524cc7e5b001241a90a
shayanalahyari <shayanforstock@gmail.com>
2024-07-14 11:41:13 +0330

    update

--- polynomial_regression/polynomial_regression.py
+++ polynomial_regression/polynomial_regression.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 
 dataset = pd.read_csv('Position_Salaries.csv')
 x = dataset.iloc[:,1:-1].values
