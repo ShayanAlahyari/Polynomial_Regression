e925fad4dd48f199e2b67c1bfc469944fe906f65
shayanalahyari <shayanforstock@gmail.com>
2024-07-14 11:36:42 +0330

    update

---
+++ polynomial_regression/polynomial_regression.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+dataset = pd.read_csv('Position_Salaries.csv')
+x = dataset.iloc[:,1:-1].values
+y = dataset.iloc[:,-1].values
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=0)
+
+linear_regressor = LinearRegression()
+linear_regressor.fit(x,y)
